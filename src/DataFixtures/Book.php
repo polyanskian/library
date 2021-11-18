a1fb37ce185fbd593c2fea5c316642298f8eeabf
pa <polyanskian@gmail.com>
2021-11-18 20:49:51 +0300

    Add fixtures for Book

---
+++ src/DataFixtures/Book.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class Book extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        for ($i = 1; $i <= 10; $i++) {
+            $book = (new \App\Entity\Book())
+                ->setName("Book $i $faker->firstName")
+                ->setAuthor($faker->lastName)
+                ->setDateRead($faker->dateTime)
+                ->setIsDownload($faker->boolean)
+            ;
+
+            $manager->persist($book);
+        }
+
+        $manager->flush();
+    }
+}
