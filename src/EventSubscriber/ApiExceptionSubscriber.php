139d6c98b47e014d0bdb4a92b6b861350da1664c
polyanskian <polyanskian@gmail.com>
2021-12-02 23:41:30 +0300

    Add subscriber handler api exceptions

---
+++ src/EventSubscriber/ApiExceptionSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ApiExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return array(
+            KernelEvents::EXCEPTION => 'onKernelException'
+        );
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $request = $event->getRequest();
+
+        if ($exception instanceof HttpExceptionInterface) {
+            return;
+        }
+
+        $route = $request->attributes->get('_route');
+
+        if (strpos($route, 'api_') !== 0) {
+            return;
+        }
+
+        $responce = new JsonResponse($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        $event->setResponse($responce);
+    }
+}
