ef9bd82b89db7427635cc0ac1f5a77a0d790b080
polyanskian <polyanskian@gmail.com>
2021-12-05 00:07:17 +0300

    Web tests add method loadFixture(). Autoload test user before getAuthorizedClient()

--- tests/CustomWebTestCase.php
+++ tests/CustomWebTestCase.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use App\DataFixtures\UserFixture;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
@@ -11,9 +12,18 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 abstract class CustomWebTestCase extends WebTestCase
 {
+    public function loadFixture(string $classFixture): self
+    {
+        $em = static::getContainer()->get(EntityManagerInterface::class);
+        $fixture = new $classFixture();
+        $fixture->load($em);
+        return $this;
+    }
+
     protected function getAuthorizedClient(): KernelBrowser
     {
         $client = static::createClient();
+        $this->loadFixture(UserFixture::class);
 
         $em = static::getContainer()->get(EntityManagerInterface::class);
         $repository = $em->getRepository(User::class);
