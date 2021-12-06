eccb5a23323793ae000da8f4d5ac331d4b3aa663
polyanskian <polyanskian@gmail.com>
2021-12-06 13:13:17 +0300

    FlashService method return self

--- src/Service/FlashService.php
+++ src/Service/FlashService.php
@@ -15,28 +15,33 @@ class FlashService
         $this->session = $session;
     }
 
-    public function notice(string $message)
+    public function notice(string $message): self
     {
         $this->session->getFlashBag()->add('secondary', $message);
+        return $this;
     }
 
-    public function ok(string $message)
+    public function ok(string $message): self
     {
         $this->session->getFlashBag()->add('success', $message);
+        return $this;
     }
 
-    public function info(string $message)
+    public function info(string $message): self
     {
         $this->session->getFlashBag()->add('info', $message);
+        return $this;
     }
 
-    public function warning(string $message)
+    public function warning(string $message): self
     {
         $this->session->getFlashBag()->add('warning', $message);
+        return $this;
     }
 
-    public function error(string $message)
+    public function error(string $message): self
     {
         $this->session->getFlashBag()->add('danger', $message);
+        return $this;
     }
 }
